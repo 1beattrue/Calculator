1aeef5482df199a3433541e098ca4438cd58bd9e
Vitaly <onebeattrue@gmail.com>
2023-09-11 20:53:42 +0300

    Задизайнено

--- app/src/main/java/edu/mirea/onebeattrue/calculator/presentation/MainActivity.java
+++ app/src/main/java/edu/mirea/onebeattrue/calculator/presentation/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import edu.mirea.onebeattrue.calculator.R;
+import edu.mirea.onebeattrue.calculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        edu.mirea.onebeattrue.calculator.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
     }
 }
